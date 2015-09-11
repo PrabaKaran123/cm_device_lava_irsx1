2f275ab0f135c57264987ef962e193cb156470bf
PrabaKaran123 <prabaecepct@gmail.com>
2015-09-11 20:40:11 +0530

    Update and rename device_x1.mk to device_irsx1.mk

--- device_irsx1.mk
+++ device_irsx1.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/lava/x1/x1-vendor.mk)
+$(call inherit-product-if-exists, vendor/lava/irsx1/irsx1-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/lava/x1/overlay
+DEVICE_PACKAGE_OVERLAYS += device/lava/irsx1/overlay
 
-LOCAL_PATH := device/lava/x1
+LOCAL_PATH := device/lava/irsx1
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_x1
-PRODUCT_DEVICE := x1
+PRODUCT_NAME := full_irsx1
+PRODUCT_DEVICE := irsx1
