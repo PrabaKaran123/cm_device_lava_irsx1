8ca8e0d2254c28909b80a6eb4fb6f7458209bc32
PrabaKaran123 <prabaecepct@gmail.com>
2015-09-12 07:21:26 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := x1
+PRODUCT_RELEASE_NAME := irsx1
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lava/x1/device_x1.mk)
+$(call inherit-product, device/lava/irsx1/device_irsx1.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := x1
-PRODUCT_NAME := cm_x1
+PRODUCT_DEVICE := irsx1
+PRODUCT_NAME := cm_irsx1
 PRODUCT_BRAND := lava
-PRODUCT_MODEL := x1
+PRODUCT_MODEL := irsx1
 PRODUCT_MANUFACTURER := lava
